44de03f9d867110cc42be6de4474d0ac63face9b
Colin Davidson <colin@pentaquine.com>
2016-12-08 22:14:30 -0800

    Add real navbar

---
+++ scrolling-nav.js
@@ -0,0 +1,20 @@
+//jQuery to collapse the navbar on scroll
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+
+function fill() {
+	var email = atob("Y29saW5AcGVudGFxdWluZS5jb20");
+	$('#emaila').prop("href", 'mailto:' + email);
+	$('#emaila2').prop("href", 'mailto:' + email);
+	$('#email').text(email);
+	$('#email2').text(email);
+}
+
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
